035507156d604777cc12c1d5429a29d2fe425786
DAM110 <DAM110@IF03-11.iesmhp.local>
2020-11-16 16:32:40 +0100

    Menu 7 opciones con las 2 primeras realizadas por Daniel Dehesa Moreno

--- src/menudam1/MenuDAM1.java
+++ src/menudam1/MenuDAM1.java
@@ -33,17 +33,18 @@ public class MenuDAM1 {
             opcion = teclado.nextInt();
             switch (opcion) {
                 case 1:
+                    //opcion del menu en la cual calcularemos si un numero es perfecto.
                     int i,
                      suma = 0,
                      n;
                     System.out.println("Introduce el número: ");
                     n = teclado.nextInt();
-                    for (i = 1; i < n; i++) {
+                    for (i = 1; i < n; i++) { // i son los divisores. Se divide desde 1 hasta n-1  
                         if (n % i == 0) {
-                            suma = suma + i;
+                            suma = suma + i; // si es divisor se suma
                         }
                     }
-                    if (suma == n) {
+                    if (suma == n) { // si el numero es igual a la suma de sus divisores es perfecto   
                         System.out.println("Perfecto");
                     } else {
                         System.out.println("No es perfecto");
